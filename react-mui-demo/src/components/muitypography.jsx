99cb200239d6d895f509a80b43b1908fdd936d81
JoanaQuadzie <joanaq03@gmail.com>
2024-04-05 10:17:36 +0000

    added the button

--- react-mui-demo/src/components/muitypography.jsx
+++ react-mui-demo/src/components/muitypography.jsx
@@ -3,23 +3,37 @@ import { Container, Typography } from "@mui/material";
 import TextFields from "@mui/material/TextField";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GoogleIcon from '@mui/icons-material/Google';
 
 export default function BasicTextFields() {
     return (
-        <Container maxWidth="sm">
-            <Typography variant='h4'>Login</Typography>
-            <Stack
-                component="form"
-                spacing={2}
-                noValidate
-                autoComplete="off"
-                
-            >
-                <TextFields id="outlined-basic" label="Username" variant="outlined"/>
-                <TextFields id="filled-basic" label="Password" variant="outlined" />
-                <TextFields id="standard-basic" label="Confirm Password" variant="outlined" />
-            </Stack>
-            <Button variant="contained">LOGIN</Button>
-        </Container>
+        <>
+            <Container maxWidth="sm">
+                <br></br>
+                <Typography variant='h4'>Login To Fresh Farms</Typography>
+                <br></br>
+                <Stack
+                    component="form"
+                    spacing={2}
+                    noValidate
+                    autoComplete="off"
+
+                >
+                    <TextFields id="outlined-basic" label="Username" variant="outlined" />
+                    <TextFields id="filled-basic" label="Password" variant="outlined" />
+                    <TextFields id="standard-basic" label="Confirm Password" variant="outlined" />
+                </Stack>
+                <br></br>
+
+                <Button variant="contained" style={{ backgroundColor: '#673ab7', color: 'white'}}>LOGIN</Button>
+
+                <br></br>
+
+            </Container>
+            <br></br>
+            <FacebookIcon/><TwitterIcon/><GoogleIcon/>
+        </>
     )
 }
